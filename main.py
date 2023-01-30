1d49a86020710a2a2e5659b7f14d52a056291306
GMuradas99 <gonzalo.muradas@odriozola.org>
2023-01-30 16:19:24 +0100

    Finished it

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2
 import random
 
-LOOP = 1
+POLYGON = 3
+LOOP = 100000
 POLYGON_POINTS = []
-DRAWN = False
 
 def draw_pixel(x,y):
     cv2.circle(img,(x,y),0,255,-1)
@@ -14,17 +14,21 @@ def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         POLYGON_POINTS.append((x,y))
         draw_pixel(x,y)
-        cv2.imshow('image',img)
     
     #Fractal
-    if len(POLYGON_POINTS) == 4:
-        triangle = POLYGON_POINTS[:3]
+    if len(POLYGON_POINTS) == POLYGON+1:
+        triangle = POLYGON_POINTS[:POLYGON]
 
         lastPoint = POLYGON_POINTS[-1]
         for i in range(LOOP):
-            
+            randPoint = random.choice(triangle)
+            newPoint = (int((randPoint[0]+lastPoint[0])/2), int((randPoint[1]+lastPoint[1])/2))
+            draw_pixel(newPoint[0],newPoint[1])
+            lastPoint = newPoint
 
         POLYGON_POINTS.clear()
+
+    cv2.imshow('image',img)
 
 img = np.zeros((512,512,1),np.uint8)
 cv2.imshow('image',img)
